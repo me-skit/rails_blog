82aa8e76b117b4ad44fe6242c66dbbe57edf5565
Meme <meme_es@hotmail.com>
2022-06-03 15:38:22 -0600

    Add eager load when the list of articles is shown

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   http_basic_authenticate_with name: "Admin", password: "Admin", except: [:index, :show]
 
   def index
-    @articles = Article.all
+    @articles = Article.all.preload(:comments)
   end
   
   def show
